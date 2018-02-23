c14d7337be107e2b9f0a5965144b2ae6fe259e7f
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-02-23 14:23:55 -0800

    working Faq component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Faq from './Faq';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Faq />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
---
+++ src/components/Faq.js
@@ -0,0 +1,11 @@
+import React from 'react'; // always import core React library
+
+function Faq(){
+  return (
+    <div>
+      <h1>FAQ</h1>
+    </div>
+  );
+}
+
+export default Faq;
