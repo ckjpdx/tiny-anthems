68ef099143619e48bf2574c9beee895bcfa2dabc
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-02-23 16:50:26 -0800

    add basic content, add logo

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,30 @@ import './styles/App.css';
 import Faq from './Faq';
 import Portfolio from './Portfolio';
 import Reviews from './Reviews';
+import Welcome from './Welcome';
 import { Switch, Route, Link } from 'react-router-dom';
+import mike from './../assets/img/mike.gif';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={mike} alt="cartoon of mike throwing up musical notes" className="mike" />
+        <h1 className="title">Tiny Anthems</h1>
+        <Link to='/'>Welcome</Link>
         <Link to='/faq'>FAQ</Link>
+        <Link to='/portfolio'>Portfolio</Link>
+        <Link to='/reviews'>Reviews</Link>
         <Switch>
-          <Route exact path='/faq' render={() =>
+          <Route exact path='/' render={() =>
+            <Welcome />} />
+          <Route path='/faq' render={() =>
             <Faq />} />
+          <Route path='/portfolio' render={() =>
+            <Portfolio />} />
+          <Route path='/reviews' render={() =>
+            <Reviews />} />
         </Switch>
-
-        <Portfolio />
-        <Reviews />
       </div>
     );
   }
--- src/components/Faq.js
+++ src/components/Faq.js
@@ -4,6 +4,42 @@ function Faq(){
   return (
     <div>
       <h1>FAQ</h1>
+      <h3>
+        How does it work?
+      </h3>
+      <p>
+        You provide whatever info you think is relevant and I make a weird song about you (or whatever topic you feel strongly about.)
+      </p>
+      <h3>
+        Why are you doing this?
+      </h3>
+      <p>
+        First it was just boredom and to practice recording/writing, but then it became about making positive, triumphant pieces of art for individuals, especially in 2018, when there's a lot of anxiety about life, generally.
+      </p>
+      <h3>
+        How do I get the song?
+      </h3>
+      <p>
+        I will email you a code to download the MP3 upon completion. Tapes and CD's available upon request for an additional fee.
+      </p>
+      <h3>
+        How much does it cost?
+      </h3>
+      <p>
+        For now, given the length of time it takes to create a piece, I ask for a $15 dollar minimum. It's crucial that cost not be prohibitive, so exceptions will be made. Listeners are encouraged to contribute more if they feel inclined.
+      </p>
+      <h3>
+        What will the song sound like?
+      </h3>
+      <p>
+        There is a huge variety of possibility for the sound. Instrumentation is likely to incorporate acoustic instruments like guitar, cello, drums, banjos, ukuleles and auxiliary percussion, and possibly electric instrumentation including synth, keyboards, electric guitar, organ. Usually the songs tend to be 2-4 minutes in accordance with pop-standards and average listener attention spans, but I'm very open to longer or shorter works.
+      </p>
+      <h3>
+        Why don't you get a real job?
+      </h3>
+      <p>
+        I've found that I'm not that employable, so I figure I'll just toil in kitchens and dishpits across the land until I die in obscurity or I make a few bucks doing this.
+      </p>
     </div>
   );
 }
