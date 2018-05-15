5fbf2a4269240bf8ce3229c404a5b659f934a36e
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-05-15 14:09:22 -0700

    deleteSong fn

--- src/components/ListedQuiz.js
+++ src/components/ListedQuiz.js
@@ -17,37 +17,22 @@ class ListedQuiz extends Component {
 		const gsReference = storage.refFromURL('gs://tiny-anthems-2043a.appspot.com/songs/' + song);
 		gsReference.delete()
 			.then(() => {
-					console.log(quiz.data.songs);
-					const newSongs = quiz.data.songs.filter(each => each !== song);
-					quiz.update({songs: newSongs});
-					console.log(newSongs);
-			})
-			.catch(err => console.error(song, quiz));
-	}
-	test(song, quiz){
-		const storage = firebase.storage();
-		const gsReference = storage.refFromURL('gs://tiny-anthems-2043a.appspot.com/songs/' + song);
-		// gsReference.delete()
-			// .then(() => {
-					console.log(quiz.data.songs);
-					const newSongs = quiz.data.songs.filter(each => {
-						console.log(each, song);
+					const oldSongs = quiz.data.songs.slice();
+					const newSongs = oldSongs.filter(each => {
 						return each !== song
 					});
-					// quiz.update({songs: newSongs});
-					console.log(newSongs);
-			// })
-			// .catch(err => console.error(song, quiz));
+					quiz.update({songs: newSongs});
+			})
+			.catch(err => console.error(song, quiz));
 	}
 
 	render() {
 		const { quiz } = this.props;
-    // console.log(name);
 		return (
       <div className="ListedQuiz-div">
         <p>{quiz.data.name} -- {quiz.data.email}</p>
 				{quiz.data.songs && quiz.data.songs.map((song, i) => {
-          return <p key={i}>{song} <span key={'x'+i} onClick={() => this.deleteSong(song, quiz)}>X</span><span key={'t'+i} onClick={() => this.test(song, quiz)}>T</span></p>
+          return <p key={i}>{song} <span key={'x'+i} onClick={() => this.deleteSong(song, quiz)}>X</span></p>
         })}
         <ListedQuizUpload quiz={quiz} />
       </div>
