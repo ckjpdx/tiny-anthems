afc1ad6b1e4bff08dd1a8decb118da1f629e2616
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-07-02 19:51:06 -0700

    non-mobile warning

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,6 +12,7 @@ function Welcome(props){
   }
   return (
     <div>
+      <p className="all-non-mobile-view-text">CURRENTLY ONLY MOBILE VIEW HAS BEEN DEVELOPED. PLEASE USE DEV TOOLS OR SHRINK SCREEN HORIZONTALLY.</p>
       <img src={tinyTape} alt="tiny anthems casette tape" className="Welcome-tape" onClick={handleTapeClick}></img>
       <p className="Welcome-tagline">...songs about you!</p>
       <img src={playerOpen} alt="open tape player" className="Welcome-player-open"></img>
