7cbc901e76b34f3c514db276236fec5c634f8002
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-11-19 13:10:45 -0800

    update portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,8 @@ class Home extends React.Component {
   render() {
     console.log(this.props);
     const loggedIn = <div>
-      <p>You're logged in!</p>
-      <p>Proceed to the user area to start an application for immortality or to download completed anthems.</p>
+      <p>Good news! You're logged in!</p>
+      <p>Proceed to the user area to start an application for immortality (an anthem) or to download completed anthems.</p>
       <Link to="/user">
         <button>Go to User Area</button>
       </Link>
@@ -19,7 +19,7 @@ class Home extends React.Component {
     const loggedOut = <div>
       <p>To accept this great quest, one must step over the threshold.</p>
       <Link to="/user">
-      <Login appState={this.props.appState} />
+        <Login appState={this.props.appState} />
       </Link>
       <p>Or if you have questions concerning this process, <Link to="/faq">click here to see the FAQ section.</Link></p>
     </div>;
@@ -28,7 +28,7 @@ class Home extends React.Component {
       <div className="Home">
         <img src={vommy} alt="cartoon of mike throwing up musical notes" id="About-vommy" />
         <h2 style={{fontStyle: 'italic'}}>“Strange Songs For Strange Humans”</h2>
-        <h3>If you’ve ever thought, “Gee, it would be neat if someone composed and recorded an overly-elaborate piece of music about me” you have arrived on the correct website. Here at Tiny Anthems, we’re in the business of distilling the ineffable nature of humans into sonic masterpieces. At last, you, too, can live forever. Be enshrined in the glory of song.</h3>
+        <h3>If you’ve ever thought, “Gee, it would be neat if someone composed and recorded an overly-elaborate piece of music about me” you have arrived on the correct website. Here at Tiny Anthems, we’re in the business of distilling the ineffable nature of humans (or animals!) into sonic masterpieces. At last, you, too, can live forever. Be enshrined in the glory of song! Listen to a collection of <Link to="/user">samples</Link> to hear some actual anthems recorded for a variety of strange humans.</h3>
         {this.props.appState.uid ? loggedIn : loggedOut}
       </div>
     );
