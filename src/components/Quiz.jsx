c59a3180df1a8c70ebd1ab114be23c3c06e5ed88
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-12-16 11:47:51 -0800

    restructure Quiz, remove submission function and local state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,105 +6,41 @@ import waves from './../assets/img/waves.png';
 import 'firebase/firestore';
 import './Quiz.css';
 import { forMeQuestionsArr, forThemQuestionsArr } from './../assets/extras/questionArr';
-import * as emailjs from 'emailjs-com';
-
-import DialogThanks from './DialogThanks';
 
 const Quiz = observer(class Quiz extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {quiz: null, dialog: false};
-    this.state.quiz = {
-      uid: props.appState.uid,
-      name: props.appState.name,
-      email: props.appState.email,
-      pending: true
-    };
-    this.state.quiz.quizData = {
-      questions: [],
-      answers: []
-    };
-    console.log(this.state);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleQuizFormSubmit = this.handleQuizFormSubmit.bind(this);
-    this.handleQuizTypeToggle = this.handleQuizTypeToggle.bind(this);
-    this.handleDialogOpen = this.handleDialogOpen.bind(this);
-    this.handleDialogClose = this.handleDialogClose.bind(this);
-  }
 
-  handleInput(e, question, i) {
-    console.log('e:', e, 'question:', question, 'i:', i);
-    const newQuiz = Object.assign({}, this.state.quiz);
-    console.log(this.state, newQuiz);
-    newQuiz.quizData.questions[i] = question;
-    newQuiz.quizData.answers[i] = e.target.value;
-    this.setState({quiz: newQuiz});
-    console.log(this.state.quiz);
-  }
-
-  handleQuizTypeToggle(e) {
-    const newQuiz = Object.assign({}, this.state.quiz);
-    newQuiz.quizType = e.target.id;
-    this.setState({quiz: newQuiz});
-    console.log(this.state.quiz);
-  }
-
-  handleQuizFormSubmit = async (e) => {
+// will need to put uid, etc into quiz on submit
+  handleQuizFormCheck = async (e) => {
     e.preventDefault();
-    const checkForAnswers = this.state.quiz.quizType === 'for-me'
+    const checkForAnswers = this.props.appState.quizType === 'for-me'
       ? forMeQuestionsArr.length
       : forThemQuestionsArr.length;
-    if (checkForAnswers === this.state.quiz.quizData.answers.length) {
-      document.getElementById("Quiz-submit").disabled = true;
-      const quizSubmitId = await quizzesCollection.add(this.state.quiz); // can return doc.id
-      if (quizSubmitId.id) {
-        emailjs.init(process.env.REACT_APP_EMAILJS);
-        const templateParams = {
-          name: this.state.quiz.name,
-          email: this.state.quiz.email
-        };
-        emailjs.send('gmail', 'template_vk6ykRLd', templateParams)
-          .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-          }, function(err) {
-            console.log('FAILED...', err);
-          });
-        this.handleDialogOpen();
-      } else {
-        alert('Something broke on the submission! We havent any idea why. Please try again later or contact Mike and tell him to yell at his web developer, Jack.');
-      }
+    if (checkForAnswers >= this.props.appState.quiz.quizData.answers.length) {
+      this.props.history.push('/user/quiz/payment');
     } else {
       alert('Please answer all the questions before submitting!');
     }
   };
 
-  handleDialogOpen = () => this.setState({dialog: true});
-  handleDialogClose = () => {
-    this.setState({dialog: false});
-    this.props.history.push('/user');
-  };
-
   render(){
-    const submitButton = <button id="Quiz-submit" onClick={this.handleQuizFormSubmit}>Immortalize!</button>;
+    const submitButton = <button id="Quiz-submit" onClick={this.handleQuizFormCheck}>Immortalize!</button>;
 
     const forMeQuestions = <div>
-      <input type="radio" checked readOnly />
-      <label>Myself</label>
       <p>
         Congratulations on taking the first step toward becoming forever enshrined in the glory of song! In order to get a sense of your personality and thus create a sonic masterpiece befitting one so noble, I’ve put together a small series of questions. The more information you provide about a subject, the better.
       </p>
       {forMeQuestionsArr.map((question, i) =>
         <div className="Quiz-for-me-question" key={'key'+i}>
           <label>{question}</label>
-          <textarea type="text" onChange={(e)=>this.handleInput(e, question, i)}/>
+          <textarea type="text"
+          value={this.props.appState.quiz.quizData.answers[i]}
+          onChange={(e)=>this.props.onQuizInput(e, question, i)}/>
         </div>
       )}
       {submitButton}
     </div>;
 
     const forThemQuestions = <div>
-      <input type="radio" checked readOnly />
-      <label>Someone Else</label>
       <p>
         Congratulations on choosing to immortalize a loved one through song. In order to paint a vivid picture of this person’s greatness, I’ll need a little information about them. I have just a few very simple questions about their identifying characteristics. Most importantly, you’ll have a chance to write freely about this person. Details might include BUT ARE ABSOLUTELY NOT LIMITED TO:
       </p>
@@ -121,7 +57,9 @@ const Quiz = observer(class Quiz extends Component {
       {forThemQuestionsArr.map((question, i) =>
         <div className="Quiz-for-them-question" key={'key'+i}>
           <label>{question}</label>
-          <textarea type="text" onChange={(e)=>this.handleInput(e, question, i)}/>
+          <textarea type="text"
+          value={this.props.appState.quiz.quizData.answers[i]}
+          onChange={(e)=>this.props.onQuizInput(e, question, i)}/>
         </div>
       )}
       {submitButton}
@@ -130,28 +68,27 @@ const Quiz = observer(class Quiz extends Component {
     return (
       <div className="Quiz">
         <h1>Questionnaire</h1>
-        {!this.state.quiz.quizType &&
+        {!this.props.appState.quizType &&
         <div>
           <p>You are about to begin your quest. Who is this for?</p>
           <div style={{position: 'relative'}}>
             <img src={waves} alt="waves" id="waves"/>
             <img src={boat} alt="a boat" id="boat"/>
           </div>
-          <p>Fill out a questionnaire for:</p>
-          <section id="Quiz-labels">
-            <div>
-              <input type="radio" id="for-me" name="for-who-buttons" onChange={this.handleQuizTypeToggle}/>
-              <label htmlFor="for-me">Myself</label>
-            </div>
-            <div>
-              <input type="radio" id="for-them" name="for-who-buttons" onChange={this.handleQuizTypeToggle}/>
-              <label htmlFor="for-them">Someone Else</label>
-            </div>
-          </section>
         </div>
         }
-        {this.state.quiz.quizType === 'for-me' ? forMeQuestions : this.state.quiz.quizType === 'for-them' ? forThemQuestions : null}
-        <DialogThanks open={this.state.dialog} onClose={this.handleDialogClose}/>
+        <p>Fill out a questionnaire for:</p>
+        <section id="Quiz-labels">
+          <div>
+            <input type="radio" id="for-me" name="for-who-buttons" onChange={this.props.onQuizType}/>
+            <label htmlFor="for-me">Myself</label>
+          </div>
+          <div>
+            <input type="radio" id="for-them" name="for-who-buttons" onChange={this.props.onQuizType}/>
+            <label htmlFor="for-them">Someone Else</label>
+          </div>
+        </section>
+        {this.props.appState.quizType === 'for-me' ? forMeQuestions : this.props.appState.quizType === 'for-them' ? forThemQuestions : null}
       </div>
     );
   }
