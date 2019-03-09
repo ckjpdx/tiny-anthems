083942f7ee9f5159852f32495c45b6f65f93d083
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-03-09 12:57:42 -0800

    update bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,13 @@ class About extends React.Component {
             <h1>About the author</h1>
             <p>What is this, exactly?</p>
             <p>
-              Some people like to party. Some people like to work on their trucks. Mike happens to enjoy assisting people in achieving immortality. Orienting himself toward the omega-point of enlightening and enlivening his community, Mike is fascinated by service, humor, music, and spirituality. Appointing himself with the humble yet vainglorious task of documenting and extolling the greatness of all 7.6 billion human residents of the planet one at a time, Mike has vowed to ensure that all beings are enshrined in the glory of song.
+              Mike settled back into the moss of the Pacific Northwest several years ago with the intention of serving his community. He works with a non-profit that rescues wasted food and delivers it to those in need. With service work as his compass, he began Tiny Anthems as a means to unify his love of music with his affinity for humans. Grounded in a humorous, philosophical framework, Mike aims to write songs that uplift and energize his community. He is very cool and you should pay him a lot. He prefers autumn to all other seasons and used to have a yellow belt in karate when he was 7, which is kind of a big deal.
             </p>
             <p>
               Mike attended The Evergreen State College and presently lives in a cramped apartment in Portland, OR.
             </p>
             <p>
-              Mike likes songs and is a proud member of the clean plate club.
-            </p>
-            <h2>
-              Other Questions? Contact Me!
-            </h2>
-            <p>
-              You can reach Mike at TinyAnthems@gmail.com, or look into the blood moon from the peak of Dark Mountain and call my name thrice. Standard rates apply.
+              You can reach Mike at TinyAnthems@gmail.com, or look into the blood moon from the peak of Dark Mountain and call his name thrice. Standard rates apply.
             </p>
           </section>
           <section>
