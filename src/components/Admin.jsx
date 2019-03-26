c073b4358272ed26fda79d682b54d520f4934314
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-03-26 13:10:28 -0700

    WIP add update prices input

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { quizzesCollection, settingsDoc } from './../store';
+import { allQuizzesCollection, quizzesCollection, settingsDoc } from './../store';
 import ProgressVommy from './common/ProgressVommy';
 import ListedQuiz from './ListedQuiz';
 import './Admin.css';
@@ -11,9 +11,11 @@ const Admin = observer(class Admin extends Component {
 	constructor(props) {
 		super(props);
 		quizzesCollection.query = quizzesCollection.ref.where('pending', '==', true);
+		allQuizzesCollection.query = allQuizzesCollection.ref.where('pending', '==', true);
 
 		this.state = {
-			filterPending: true
+			filterPending: true,
+			newPrices: null
 		};
 	}
 
@@ -39,17 +41,43 @@ const Admin = observer(class Admin extends Component {
 		});
 	}
 
+	changePrices = (e) => {
+		if (e.target.value !== "") {
+			const arr = e.target.value.match(/\d+/gm);
+			if (arr) {
+				const newPriceArr = arr.map(price => Number(price));
+				this.setState({newPrices: newPriceArr});
+			}
+		} else {
+			this.setState({newPrices: null});
+		}
+	}
+
 	render() {
 		const { docs } = quizzesCollection;
 		const listedQuizzes = docs.map((quiz) => <ListedQuiz key={quiz.id} quiz={quiz} />);
-		const wait = settingsDoc.data.waitPerSong;
+		const perSongWait = settingsDoc.data.waitPerSong;
+		const currentWaitTime = allQuizzesCollection.docs.length * settingsDoc.data.waitPerSong;
+		const prices = settingsDoc.data.prices;
+		const nPrices = this.state.newPrices;
 
 		return (
 			<div className="Admin">
   			<h1><FontAwesomeIcon icon={faCrown} /> ADMIN <FontAwesomeIcon icon={faCrown} /></h1>
 				{quizzesCollection.fetching ? <ProgressVommy /> :
 				<div>
-					<label>Wait factor: X{wait} (days) </label><button onClick={this.changeWait} value="1">+</button><button onClick={this.changeWait} value="-1">-</button>
+					<label>Wait: P x {perSongWait} = {currentWaitTime} days </label>
+					<button onClick={this.changeWait} value="1">+</button>
+					<button onClick={this.changeWait} value="-1">-</button>
+					<p className="subtext">Days wait per song x pending songs = total wait time</p>
+					<div>
+						<label>Prices: {prices && prices.join(", ")} </label>
+						{nPrices && <label style={{color: 'red'}}>New: {nPrices.join(", ")}</label>}
+						<br/>
+						<input onChange={this.changePrices}/>
+						{nPrices && <button>Update</button>}
+						<p className="subtext">New prices separated by commas: 50, 100, 200</p>
+					</div>
 					<div style={{margin: 10}}>
 						<label>Filter for:</label>
 						<input type="checkbox" checked={this.state.filterPending} onChange={this.changeFilterPending.bind(this)}/><label>Pending</label>
