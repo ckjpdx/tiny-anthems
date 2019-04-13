3eda80f2fe06041fabc637e4236a7a3097a4790c
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-04-13 14:56:07 -0700

    update questions

--- src/assets/extras/questionArr.js
+++ src/assets/extras/questionArr.js
@@ -2,6 +2,5 @@ export const questionsArr = [
 `Who is the subject of the song?`,
 `Tell me about the subject! (This is the part where you essay exhaustively)`,
 `Can you describe your relationship to the subject?`,
-`Do you have any idea how you might want the piece to sound?`,
 `Is this piece for a specific occasion?`,
 ];
