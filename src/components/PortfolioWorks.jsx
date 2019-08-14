d583a4d73bd177f07ea23f7f342cfe74703b9d08
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-08-14 12:26:07 -0700

    connect Players to uploaded full song examples

--- src/components/PortfolioWorks.jsx
+++ src/components/PortfolioWorks.jsx
@@ -9,8 +9,33 @@ function PortfolioWorks(){
     <div>
       <p>Works will go here</p>
         <Player
-        trackTitle="Alanna, Supergood At Literally Everything (Let Us Just Go Ahead And Elect Her President)"
-        streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/songs%2FAlanna%2C%20Supergood%20At%20Literally%20Everything%20(Let%20Us%20Just%20Go%20Ahead%20And%20Elect%20Her%20President).mp3?alt=media&token=8c7f5945-3a2f-40c4-9179-99f41761841e"/>
+          trackTitle="Bill and Penni, Tolerating One Another for 26,000,000 Minutes"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FBill%20and%20Penni%2C%20Tolerating%20One%20Another%20for%2026%2C000%2C000%20Minutes.mp3?alt=media&token=87b1fe12-47a8-45b0-9de4-110567b219e1"
+        />
+        <Player
+          trackTitle="Cait, Who Never Surrendered To Law Enforcement (She's No Criminal)"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FCait%2C%20Who%20Never%20Surrendered%20To%20Law%20Enforcement%20(She_s%20No%20Criminal).mp3?alt=media&token=7dcd106a-2c6e-41bc-904f-e5648fd4a113"
+        />
+        <Player
+          trackTitle="Dave, Specializing In Felines and Sports, But Not At The Same Time"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FDave%2C%20Specializing%20In%20Felines%20and%20Sports%2C%20But%20Not%20At%20The%20Same%20Time.mp3?alt=media&token=b67dd6ba-7b18-4d2f-9543-69ca205a8116"
+        />
+        <Player
+          trackTitle="Gina, The Chomatic Saint Against Whom All Flames Lap Harmlessly"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FGina%2C%20The%20Chomatic%20Saint%20Against%20Whom%20All%20Flames%20Lap%20Harmlessly.mp3?alt=media&token=72aaa73d-b92a-4b2f-af57-8f05f2bccc92"
+        />
+        <Player
+          trackTitle="Harvey, The World is in Your Capable Care"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FHarvey%2C%20The%20World%20is%20in%20Your%20Capable%20Care.mp3?alt=media&token=b14f38b8-7047-4538-8d09-988196bbae98"
+        />
+        <Player
+          trackTitle="Portland Monthly, You Delivered Us From Our Waking Nightmare (Let Us Rejoice In Song)"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FPortland%20Monthly%2C%20You%20Delivered%20Us%20From%20Our%20Waking%20Nightmare%20(Let%20Us%20Rejoice%20In%20Song).mp3?alt=media&token=60601868-d1f8-4179-8f6b-3e42a9e6c33d"
+        />
+        <Player
+          trackTitle="Roons, Portland's Confectionary Miracle That Lifted Me From The Wretched Sinkhole That Was My Old Life (A Review)"
+          streamUrl="https://firebasestorage.googleapis.com/v0/b/tiny-anthems-2043a.appspot.com/o/examples%2FRoons%2C%20Portland_s%20Confectionary%20Miracle%20That%20Lifted%20Me%20From%20The%20Wretched%20Sinkhole%20That%20Was%20My%20Old%20Life%20(A%20Review).mp3?alt=media&token=4007dac5-4a02-4e61-bdc9-cec663c81ee7"
+        />
     </div>
   );
 }
