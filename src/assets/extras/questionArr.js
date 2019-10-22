17eca62f68c6284e2c883cc02d3681b082ebad0b
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-10-21 18:45:47 -0700

    small text update

--- src/assets/extras/questionArr.js
+++ src/assets/extras/questionArr.js
@@ -1,5 +1,5 @@
 export const questionsArr = [
-`Who is the subject of the song?`,
+`Who is the subject of the song? (Keep this short, as either a name or title)`,
 `Tell me about the subject! (This is the part where you essay exhaustively)`,
 `Can you describe your relationship to the subject?`,
 `Is this piece for a specific occasion?`,
