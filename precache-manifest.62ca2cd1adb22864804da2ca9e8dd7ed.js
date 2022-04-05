ec835ca8d88bd1831306aeda4c93e7b932250603
Chris Knight Johnson <cknightjohnson@gmail.com>
2022-04-05 15:11:01 -0700

    Updates

---
+++ precache-manifest.62ca2cd1adb22864804da2ca9e8dd7ed.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecb90a84bee589a27f11062a145fda1",
+    "url": "/tiny-anthems/index.html"
+  },
+  {
+    "revision": "c4d9cda523344f909c76",
+    "url": "/tiny-anthems/static/css/main.b48a1a18.chunk.css"
+  },
+  {
+    "revision": "ffa40436baf32f0efa95",
+    "url": "/tiny-anthems/static/js/2.f64744c9.chunk.js"
+  },
+  {
+    "revision": "3f8c44e8f48a8492da0678a41f85b0d1",
+    "url": "/tiny-anthems/static/js/2.f64744c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4d9cda523344f909c76",
+    "url": "/tiny-anthems/static/js/main.ebdbfde9.chunk.js"
+  },
+  {
+    "revision": "4f776072c0cc9f3993c6",
+    "url": "/tiny-anthems/static/js/runtime-main.3e575bdb.js"
+  },
+  {
+    "revision": "ef2fba66d8a1f5f27d4807f6a88f8b9d",
+    "url": "/tiny-anthems/static/media/balloon.ef2fba66.gif"
+  },
+  {
+    "revision": "2575493c33d1860da8d648fdbeed12cc",
+    "url": "/tiny-anthems/static/media/boat.2575493c.png"
+  },
+  {
+    "revision": "6d1222907a015f9b3291f7a3da9044e0",
+    "url": "/tiny-anthems/static/media/butterfly.6d122290.png"
+  },
+  {
+    "revision": "7c4cea93e62c98431ef670676a9c4c1c",
+    "url": "/tiny-anthems/static/media/cacoon.7c4cea93.png"
+  },
+  {
+    "revision": "14e5aa90bbbe7ebb0f61edec6d08521a",
+    "url": "/tiny-anthems/static/media/castleflag.14e5aa90.gif"
+  },
+  {
+    "revision": "e5eefb5fa5738fd56023787fd3b41734",
+    "url": "/tiny-anthems/static/media/catterpillar.e5eefb5f.gif"
+  },
+  {
+    "revision": "e1e924bd853746f80ab3c45b31f82aeb",
+    "url": "/tiny-anthems/static/media/donation.e1e924bd.png"
+  },
+  {
+    "revision": "8b9da916555d0c6a56684ea17912d1b2",
+    "url": "/tiny-anthems/static/media/ghost.8b9da916.png"
+  },
+  {
+    "revision": "7c039057deb4a95432edbdf61900aa3b",
+    "url": "/tiny-anthems/static/media/lighthouse-rays.7c039057.gif"
+  },
+  {
+    "revision": "918a9f3fdb63c53c5722fb2751355c72",
+    "url": "/tiny-anthems/static/media/lighthouse.918a9f3f.png"
+  },
+  {
+    "revision": "551cab14938ef40aa8c7104242de5a25",
+    "url": "/tiny-anthems/static/media/mike.551cab14.gif"
+  },
+  {
+    "revision": "9d13f7146b8b25f3aee3caa5682f66ff",
+    "url": "/tiny-anthems/static/media/paper-plain-page.9d13f714.jpg"
+  },
+  {
+    "revision": "f2e8a282a75343671a0547f78333a000",
+    "url": "/tiny-anthems/static/media/payment-time.f2e8a282.gif"
+  },
+  {
+    "revision": "d17c0486b0621301ba1423e6bb5b320c",
+    "url": "/tiny-anthems/static/media/rider.d17c0486.gif"
+  },
+  {
+    "revision": "10fb074a3a5b999e4cc1041468950cfd",
+    "url": "/tiny-anthems/static/media/robot.10fb074a.gif"
+  },
+  {
+    "revision": "cf8f557176346497e4f68b77585bb07d",
+    "url": "/tiny-anthems/static/media/slug.cf8f5571.png"
+  },
+  {
+    "revision": "602a092f7868e698b9d49db19f0edad3",
+    "url": "/tiny-anthems/static/media/tiny-theme.602a092f.mp3"
+  },
+  {
+    "revision": "7fa3b81b1fe7045751bf9d4e373f3e7f",
+    "url": "/tiny-anthems/static/media/tiny_metal_player_front_closed.7fa3b81b.png"
+  },
+  {
+    "revision": "01658da1b0231a3db7ebe46015c0ff9a",
+    "url": "/tiny-anthems/static/media/tiny_metal_player_front_open_door.01658da1.png"
+  },
+  {
+    "revision": "8dc7c0fbc78621cba9289086d13db0b8",
+    "url": "/tiny-anthems/static/media/tiny_player_jack_cover.8dc7c0fb.png"
+  },
+  {
+    "revision": "33ab6646b4010442e8b859fe4ffebf01",
+    "url": "/tiny-anthems/static/media/tiny_player_jack_empty.33ab6646.png"
+  },
+  {
+    "revision": "29ad75fc5453df3dc7eb80657e015c15",
+    "url": "/tiny-anthems/static/media/tiny_sharpie.29ad75fc.png"
+  },
+  {
+    "revision": "a65c1b18da8fe0401dcdf282e267e2aa",
+    "url": "/tiny-anthems/static/media/tiny_tape_nowheels.a65c1b18.png"
+  },
+  {
+    "revision": "5d2742fe6ca5c9cdbd0127b181e93ca7",
+    "url": "/tiny-anthems/static/media/tiny_tape_wheel_left.5d2742fe.png"
+  },
+  {
+    "revision": "8f64f17144ab8b1cfedd85cf31b356e9",
+    "url": "/tiny-anthems/static/media/tiny_tape_wheel_right.8f64f171.png"
+  },
+  {
+    "revision": "106376fa5f18f3fc60bd7c35c646242d",
+    "url": "/tiny-anthems/static/media/vommypin.106376fa.png"
+  },
+  {
+    "revision": "a440dc16f5d2fbbc2a1bfe425bf7933b",
+    "url": "/tiny-anthems/static/media/waves.a440dc16.png"
+  }
+]);
